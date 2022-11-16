4d9ebc2612424c75360cde4c1c83aeb7ab196818
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-16 20:58:26 +0500

    info about exit from the endless loop

---
+++ exit2/Program.cs
@@ -0,0 +1,10 @@
+﻿for (int i = 1; i > 0; i++)
+{
+    Console.WriteLine(i);
+    string? input = Console.ReadLine();
+    string? text = "exit";
+    if (input == text)
+    {
+        break;
+    }
+}
