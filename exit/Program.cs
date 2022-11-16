10c695adb3de6d9874162beefed67494c7cd8cde
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-16 20:53:12 +0500

    added info about exit

---
+++ exit/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается */
+
+Console.Write("Введите любую цифру: ");
+string? number = Console.ReadLine();
+while (number != "exit")
+{
+Console.Write("Введите любую цифру: ");
+number = Console.ReadLine();
+}
