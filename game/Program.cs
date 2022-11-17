78af08a351406d0a23cfacb7a281be2fb31974fc
Tata <115809153+Tatyana1774@users.noreply.github.com>
2022-11-17 18:30:14 +0500

    изменила ответы системы

--- game/Program.cs
+++ game/Program.cs
@@ -2,40 +2,37 @@
 то программа сообщает, больше загаданное число или меньше. На отгадывание дается 3 попытки.*/
 
 Console.WriteLine("Введите число от 0 до 10: ");
-    int randomNumber = new Random().Next(0, 10);
-    int attempts = 3;  
-    Console.WriteLine($"Колличество попыток {attempts}");
+int randomNumber = new Random().Next(0, 10);
+int attempts = 3;  
+Console.WriteLine($"Колличество попыток {attempts}");
 
-    for(int i = 1; i <= attempts; i++)
-    while (true)
-      {               
-      Console.WriteLine("Введите число");
-      int number = Convert.ToInt32(Console.ReadLine());
-      if (number == randomNumber)
-      {
-      Console.WriteLine($"Вы угадали");
-      break;
-      }
-      else if(number > randomNumber)
-      {
-      Console.WriteLine($"Неверно! Загаданное число меньше, чем {number}");
-      break;
-      }
-      else
-      {
-      Console.WriteLine($"Неверно! Загаданное число больше, чем {number}");
-      break;
-      }
-        if (i == attempts)
-        {
-        Console.WriteLine("Попытки закончились");
-        break;
-        }
-        if(number != randomNumber)
-        {
-        Console.WriteLine($"Осталось попыток: {attempts - i}");
-        Convert.ToInt32(Console.ReadLine());
-        break;
-        }
-      }
+for(int i = 1; i <= attempts; i++)
+while (true)
+{
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == randomNumber)
+{
+Console.WriteLine($"Вы угадали! Осталось {attempts-i} попыток");
+}
+
+    else if (number > randomNumber)
+    {
+    Console.WriteLine($"Неверно! Загаданное число меньше, чем {number}! Осталось {attempts-i} попыток");
+    break;
+    }
+    else
+    {
+    Console.WriteLine($"Неверно! Загаданное число больше, чем {number}!  Осталось {attempts-i} попыток");
+    break;
+    }
+if (number == randomNumber)
+    {
+    if (i == attempts)
+    {
+    Console.WriteLine("Попытки закончились");
+    break;
+    }
+  }
+}
 
