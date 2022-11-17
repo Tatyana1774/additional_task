8cb98aede814312b72e511baa435195fbc5b2754
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-17 11:16:36 +0500

    info about game added

---
+++ game/Program.cs
@@ -0,0 +1,38 @@
+﻿/*3. Игра угадайка. Программа загадывает случайное число. Пользователь его угадывает. Если пользователь дает неправильный ответ, 
+то программа сообщает, больше загаданное число или меньше. На отгадывание дается 3 попытки.*/
+
+Console.WriteLine("Введите число от 0 до 10: ");
+    int randomNumber = new Random().Next(0, 10);
+    int attempts = 3;  
+    Console.WriteLine($"Колличество попыток {attempts}");
+
+    for(int i = 1; i <= attempts; i++)
+    while (true)
+      {               
+      Console.WriteLine("Введите число");
+      int number = Convert.ToInt32(Console.ReadLine());
+      if (number == randomNumber)
+      {
+      Console.WriteLine($"Вы угадали c {attempts} попытки:");
+      break;
+      }
+      else if(number > randomNumber)
+      {
+      Console.WriteLine("Ваше число больше");
+      }
+      else
+      {
+      Console.WriteLine("Ваше число меньше");
+      }
+        if(number != randomNumber)
+        {
+        Console.WriteLine($"Неверно! Осталось попыток: {attempts - i}");
+        Convert.ToInt32(Console.ReadLine());
+        break;
+        }
+          if (i == attempts)
+          {
+          Console.WriteLine("Попытки закончились");
+          break;
+          }
+      }
